4f43db8a812258c5a3e6f1f92d9af4acb06ce6ec
Rakshith Kunchum <settyblue@gmail.com>
2016-11-16 13:09:10 -0800

    Solution to sorted subsegments problem. facing runtime issues for few test cases.

---
+++ Algorithms/Search/sortedSubsegments.java
@@ -0,0 +1,33 @@
+/**
+ * https://www.hackerrank.com/challenges/sorted-subsegments
+ */
+
+
+/**
+ * @author rakshith
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int q = input.nextInt();
+        int k = input.nextInt();
+        int[] array = new int[n];
+        for(int i=0; i<n; i++){
+            array[i] = input.nextInt();
+        }
+        for(int i=0; i<q; i++){
+            int r = input.nextInt();
+            int s = input.nextInt();
+            Arrays.sort(array,r,s+1);
+        }
+        //System.out.println(Arrays.toString(array));
+        System.out.println(array[k]);
+    }
+}
